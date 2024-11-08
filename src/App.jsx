6ef4fa45a2ab3a6fdef0e6c3f51d258d0d95ad23
Joaquín Cavitelli <75315685+JoaquinCaviltelli@users.noreply.github.com>
2024-11-08 15:12:28 -0300

    v5

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
 // src/App.jsx
 import { Routes, Route } from "react-router-dom";
 import { useAuth } from "./contexts/AuthContext";
+import { Navigate } from "react-router-dom";
 import AdminPage from "./pages/AdminPage";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import PhonePage from "./pages/PhonePage";
 import ClientsPage from "./pages/ClientsPage";
 import ServicesPage from "./pages/ServicesPage";
-import Menu from "/src/pages/Menu.jsx";
 import WorkHoursPage from "./pages/WorkSchedulePage"; // Importar la nueva página de horarios
 import NonWorkingDaysPage from "./pages/NonWorkingDaysPage"; // Importar la nueva página de horarios
 import Spinner from "./components/Spinner";
 // En tu archivo index.js o App.js
-import '@fortawesome/fontawesome-free/css/all.min.css';
-
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 function App() {
   const { user, loading } = useAuth();
@@ -24,17 +23,16 @@ function App() {
 
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/admin" element={<AdminPage />}>
-        <Route path="clients" element={<ClientsPage />} />
-        <Route path="services" element={<ServicesPage />} />
-        <Route path="work-hours" element={<WorkHoursPage />} /> {/* Nueva ruta para horarios laborales */}
-        <Route path="NonWorkingDaysPage" element={<NonWorkingDaysPage />} /> {/* Nueva ruta para horarios laborales */}
-        <Route path="Menu" element={<Menu />} /> {/* Nueva ruta para horarios laborales */}
-      </Route>
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/phone" element={<PhonePage />} />
-    </Routes>
+    <Route path="/" element={<HomePage />} />
+    <Route path="/admin" element={user ? <AdminPage /> : <Navigate to="/login" />}>
+      <Route path="clients" element={user ? <ClientsPage /> : <Navigate to="/login" />} />
+      <Route path="services" element={user ? <ServicesPage /> : <Navigate to="/login" />} />
+      <Route path="work-hours" element={user ? <WorkHoursPage /> : <Navigate to="/login" />} />
+      <Route path="non-working-days" element={user ? <NonWorkingDaysPage /> : <Navigate to="/login" />} />
+    </Route>
+    <Route path="/login" element={<LoginPage />} />
+    <Route path="/phone" element={<PhonePage />} />
+  </Routes>
   );
 }
 
